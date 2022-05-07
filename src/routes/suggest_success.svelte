1b31880d7315249506db21f86efed33cf35e92fd
alexgoodison <alexandergoodison@gmail.com>
2022-05-07 16:54:41 +0100

    Added suggest prompt to submit success page

--- src/routes/suggest_success.svelte
+++ src/routes/suggest_success.svelte
@@ -12,10 +12,13 @@
   <div class="max-w-3xl mx-auto px-8 text-center">
     <div class="text-red-500 font-bold text-5xl mb-4">Success!</div>
     <div class="text-2xl mb-8">Your defibrillator location was submitted successfully! We'll keep you updated via email.</div>
-    <a href="/" class="rounded-lg px-3 py-2 bg-gray-200 text-gray-600 font-semibold hover:bg-gray-300 transition ease-in-out duration-150">Back to map</a>
+    <div class="flex gap-4 justify-center">
+      <a href="/" class="rounded-lg px-3 py-2 bg-gray-200 text-gray-600 font-semibold hover:bg-gray-300 transition ease-in-out duration-150">Back to map</a>
+      <a href="/suggest" class="rounded-lg px-3 py-2 bg-gray-200 text-gray-600 font-semibold hover:bg-gray-300 transition ease-in-out duration-150">Suggest another defibrillator</a>
+    </div>
   </div>  
 
-  <div class="flex justify-center p-8">
+  <div class="flex justify-center pt-24 p-8">
     <Footer />
   </div>
 </div>
